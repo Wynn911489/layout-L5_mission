501eed720c31009bc5001eeeafe08877d9ac9a19
Wynn <wynn911489@gmail.com>
2026-08-05 01:44:25 +0800

    fix: update base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法:
   // base: '/Repository 的名稱/'
-  base: '/L5-misson',
+  base: '/layout-L5_misson',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
