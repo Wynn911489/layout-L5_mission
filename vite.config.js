6b3c5520ce6cbc2154b8bbd16973adcb304ef717
Wynn <wynn911489@gmail.com>
2026-08-20 01:37:20 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法:
   // base: '/Repository 的名稱/'
-  base: '/layout-L5_misson',
+  base: '/layout-L5_mission',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
